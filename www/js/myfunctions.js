a8f0e1c81598b1ebe81fd77c21b1b438d5826938
Ali Efe <efeali@gmail.com>
2012-12-17 18:44:02 -0800

    dec 17 6:43pm
    addition to navigation, contact functions added

--- www/js/myfunctions.js
+++ www/js/myfunctions.js
@@ -109,3 +109,76 @@ function stopServer()
 {
 	alert('Updating server stopped');
 }
+
+function saveContact()
+{
+	var details = { 
+		"FirstName" : document.getElementById('contactName').value,
+		"LastName" : document.getElementById('contactLName').value,
+		"FullName" : document.getElementById('contactName').value + " " + document.getElementById('contactLName').value,
+		"EmailAddress" : document.getElementById('contactEmail').value,
+		"MobilePhone" : document.getElementById('contactPhone').value	
+	}
+	
+	var contact = navigator.contacts.create();
+	contact.displayName = details.FullName;
+	
+	var tmpName = new ContactName();
+	tmpName.givenName = details.FirstName;
+	tmpName.familyName = details.LastName;
+	tmpName.formatted = details.FullName;
+	contact.name = tmpName;
+	
+	var phoneNums = [1];
+	phoneNums[0] = new ContactField('mobile', details.MobilePhone, true);
+	contact.phoneNumbers = phoneNums;
+	
+	var emailAddresses = [1];
+	emailAddresses[0] = new ContactField('home', details.EmailAddress, true);
+	contact.emails = emailAddresses;
+	
+	contact.save(function(){alert('New contact was saved'); $.mobile.changepage("#home",{ transition:"slideup"}); }, function(){alert('Contact couldn\'t saved');});
+}
+
+function searchContact()
+{
+	var name = document.getElementById('contactName').value;
+	var opts = new ContactFindOptions();
+	opts.filter = name;
+	opts.multiple = true;
+	var fields = ["displayName", "name","phoneNumbers"];
+	navigator.contacts.find(fields, contactFindSuccess, function(){alert('Contact search error');}, opts);
+}
+function contactFindSuccess(contacts)
+{
+	var numContacts = contacts.length;
+	if(numContacts > 0)
+	{
+
+		//console.log(contacts);
+		var content = "";
+		
+		var theList = '<ul data-role="collapsible">';
+		for(var i =0; i<numContacts; i++)
+		{
+			theList += '<li><a href="#" data-role="button">'+contacts[i].name.givenName+' '+contacts[i].name.familyName+'</a>';
+			theList += '<ul data-role="listview"><li>'+contacts[i].name.givenName+' '+contacts[i].name.familyName+'</li>';
+			if(contacts[i].phoneNumbers.length>0)
+			{
+				for(var x=0; x<contacts[i].phoneNumbers.length; x++)
+				{
+					theList +='<li><span>'+contacts[i].phoneNumbers[x].value+'</span> '+contacts[i].phoneNumbers[x].type+'</li>';
+				}
+			}
+			theList +='</ul>';
+			theList += '</li>';
+		}
+		theList += '</ul>';
+		$('#contactsList').html(theList).css('display','block');
+		
+		$('#contactsList li a').click(function(){
+			$(this).parent('li').children('ul').toggle();
+		});
+		
+	}
+}
