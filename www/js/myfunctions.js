9b72f36305289813ea69311bc378b181dc6018e0
danactive <githubcom@domaindesign.ca>
2015-02-05 15:03:03 -0800

    week9

--- www/js/myfunctions.js
+++ www/js/myfunctions.js
@@ -1,284 +1,243 @@
-var states = {}; // for connection state
-var watchID, geoWatchID, serverUpdateTimer, imageURI; // this will be used for compass
-var myPosition;
-var userID = 1;
+var navOptions = {enableHighAccuracy:true, timeout:15000, maximumAge:180000};
+var locationID ; // we will use this to store watchPosition s id so we can cancel it when we want.
+var user;
+var client = new XMLHttpRequest(); // prepare asynchronous request javascript client (ajax), this will be used to save a post (and upload an image)
+
+///// define variables we will use for webSQL database
+var localdb, localdbReady;
+var localdbVersion = 1;
+var localdbName = "webblog";
+var localdbDisplayName = "webblog_db";
+var localdbSize = 2*1024*1024;
+/// end of db variables
+
+var postTimer; // this will be used for storing interval timer id
+
+var online = navigator.onLine; // navigator.onLine contains boolean value for if you have network connection or not
+
+window.addEventListener('online', function(){ // when you become online
+   online = true;
+    alert('you are online');
+});
+
+window.addEventListener('offline', function(){ // when you become offline
+    online = false;
+    alert('you are offline');
+});
+
+window.addEventListener('load',function(){
+    FastClick.attach(document.body); // this will attach and activate fastclick hack
 
 
-function checkConnection()
+
+    user = JSON.parse(sessionStorage.getItem('user')); // we have stored user object as text in session once we logged in, now when this page loading we are reading what was stored, and convert that to javascript object
+    // this object already has id property
+
+    if(user == null)
+    {
+        document.location.href = "index.html";
+    }
+    // create or maintain local database
+    localdb = window.openDatabase(localdbName, localdbVersion, localdbDisplayName, localdbSize, function(){ createLocalTable();});
+    // after database created we call createLocalTable() function to create tables if they are not exists
+
+
+});
+
+$('#home').on('pageshow',function(){
+
+    getPosts();
+    postTimer = setInterval(getPosts, 60000);
+});
+
+$('#addPost').on('pageshow',function(){
+    locationID = navigator.geolocation.getCurrentPosition(watchLocation, locationError, navOptions);
+    // start detecting our location when send post page loaded
+});
+
+$('#addPost').on('click',function(){
+    $('#topMenu').collapsible("collapse");
+});
+
+$(document).on("pagebeforehide","#home",function(){ // When leaving pagetwo
+    clearInterval(postTimer);
+    $('#topMenu').collapsible("collapse");
+});
+
+$('#sendBtn').on('click', function(){ sendPost(); }); // in addpost page if we click send button we will // call sendPost function
+
+$('.logoutBtn').on('click',function(){
+    $('#topMenu').collapsible('collapse'); // closing menu
+
+    $.get("http://vancouverwebschool.com/webblog/admin/handler.php",{logout:true}); // send rest request to server for logging out
+   document.location.href = "index.html";
+});
+
+$(document).on("click",function(){
+    $('#topMenu').collapsible("collapse");
+});
+
+////// LOCAL DATABASE
+function createLocalTable()
 {
-	var networkState = navigator.connection.type;
-	states[Connection.UNKNOWN]  = 'Unknown connection';
-	states[Connection.ETHERNET] = 'Ethernet connection';
-	states[Connection.WIFI] = 'Wifi connection';
-	states[Connection.CELL_2G] = 'Cell 2G connection';
-	states[Connection.CELL_3G] = 'Cell 3G connection';
-	states[Connection.CELL_4G] = 'Cell 4G connection';
-	states[Connection.NONE] = 'No network connection';
-	
-	return ('Connection type : ' + states[networkState]);
+    var sql1 = "CREATE TABLE IF NOT EXISTS photos (photoID int(10) DEFAULT NULL, filename varchar(100) DEFAULT NULL, postID int(10) DEFAULT NULL);";
+
+    var sql2 = "CREATE TABLE IF NOT EXISTS posts (postID int(10) NOT NULL PRIMARY KEY, content text DEFAULT NULL, date timestamp NOT NULL, authorID int(10) NOT NULL, comments int(11) NOT NULL DEFAULT 0, locLatitude float DEFAULT NULL, locLongitude float DEFAULT NULL);";
+
+    var sql3 = "CREATE TABLE IF NOT EXISTS users (userID int(10) NOT NULL PRIMARY KEY, userName varchar(50) NOT NULL);";
+
+    localdb.transaction(function(tx){
+        tx.executeSql(sql1);
+        tx.executeSql(sql2);
+        tx.executeSql(sql3);
+    });
+    localdbReady = true; // setting flag so when we get posts we will insert latest 20 into localdb
 }
+//////  end of createLocalTable()
 
-function showDeviceName()
-{
-	$('#home div p').html(window.device.name);
-}
+function sendPost(){
+    var content = $('#postContent').val(); // get post content
+    var file = document.getElementById('postPhoto'); // get file element
+    var formData = new FormData(); // prepare a new form
 
-$(document).delegate("#connection","pageinit", function(){
-	$('#connection div[data-role=content]').html(checkConnection());
-})
+    formData.append('postContent', content); // append content into this new form
+    formData.append('postAuthorID', user.id); // append user id into form
+    formData.append('postLat', user.lat); // append element named postLat and put user's latitute value
+    formData.append('postLong', user.long);
 
-$(document).delegate("#myDevice","pageinit",function(){
-
-	var content = "";
-	content += 'Device Name : '+ device.name + '<br />';
-	content += 'Device Cordova : ' + device.cordova + '<br />';
-	content += 'Device Platform : ' + device.platform + '<br />';
-	content += 'Device UUID : ' + device.platform + '<br />';
-	content += 'Device Version : ' + device.version + '<br />';
-	$('#deviceInfo').html(content);
-	
-})
-
-$(document).delegate("#navAndCompass", "pageinit", function(){
-	watchID = navigator.compass.watchHeading(compSuccess, compError, { frequency :5000 });
-	geoWatchID = navigator.geolocation.watchPosition(navSuccess, navError, {timeout:30000, maximumAge:30000, enableHighAccuracy:true});
-	
-
-})
-
-$(document).delegate("#cameraPage", "pageinit", function(){
-	$('#imageStatus').css('display','none');
-})
-
-window.addEventListener("batterystatus", showBatteryLevel, false);
-document.addEventListener("deviceready", onDeviceReady, false);
-
-function onDeviceReady(){
-	
-	
-}
-function compSuccess(heading)
-{
-	$('#compass').html('Heading :' + heading.magneticHeading);
-}
-function compError(compassError)
-{
-	$('#compass').html('compass error : '+compassError.code);
-}
-
-function navSuccess(position)
-{
-	var content = "";
-	content += 'Latitude: ' + position.coords.latitude + '<br />';
-	content += 'Longitude: ' + position.coords.longitude +'<br />';
-	content += 'Altitude: ' + position.coords.altitude +'<br />';
-	content += 'Accuracy: ' + position.coords.accuracy +'<br />';
-	content += 'Altitude accuracy: ' + position.coords.altitudeAccuracy +'<br />';
-	content += 'Heading: ' + position.coords.heading +'<br />';
-	content += 'Speed: ' + position.coords.speed +'<br />';
-	$('#myNavigator').html(content);
-	myPosition = position;
-	
-}
-function navError(error)
-{
-	$('#myNavigator').html('Code :' +error.code +'<br />Message : '+error.message);
-}
-
-
-function showBatteryLevel(info)
-{
-	var content = '';
-	content += 'Current battery level : ' + info.level + '<br />';
-	if(info.isPlugged == true)
-		content += 'Device is plugged to power';
-	else
-		content += 'Device is not plugged to power';
-	$('#batteryInfo').html(content);
-}
-
-function updateServer(obj)
-{
-	var pos = new Array();
-	navigator.geolocation.getCurrentPosition(navSuccess, navError, {timeout:30000, maximumAge:30000, enableHighAccuracy:true});
-	$('#updateStopButton').css('display','block');
-	$('#updateStartButton').css('display','none');
-	$('#serverUpdateStatus').html('Server update is active').css('color','#C00');
-	pos = myPosition.coords;
-	$.ajax({
-		type:"POST",
-		cache:false,
-		timeout:3000,
-		url:"http://theiamzone.com/ali_efe/mobile-app/api-port.php",
-		data:{navUp:1,position:pos, uid:userID, dev:device.name}
-	})
-	serverUpdateTimer = setTimeout(updateServer,120000);
-	
-}
-function stopServer()
-{
-		clearTimeout(serverUpdateTimer);
-		$('#updateStopButton').css('display','none');
-		$('#updateStartButton').css('display','block');
-		$('#serverUpdateStatus').html('Server update stopped').css('color','#069');
-}
-
-function saveContact()
-{
-	var details = { 
-		"FirstName" : document.getElementById('contactName').value,
-		"LastName" : document.getElementById('contactLName').value,
-		"FullName" : document.getElementById('contactName').value + " " + document.getElementById('contactLName').value,
-		"EmailAddress" : document.getElementById('contactEmail').value,
-		"MobilePhone" : document.getElementById('contactPhone').value	
-	}
-	
-	var contact = navigator.contacts.create();
-	contact.displayName = details.FullName;
-	
-	var tmpName = new ContactName();
-	tmpName.givenName = details.FirstName;
-	tmpName.familyName = details.LastName;
-	tmpName.formatted = details.FullName;
-	contact.name = tmpName;
-	
-	var phoneNums = [1];
-	phoneNums[0] = new ContactField('mobile', details.MobilePhone, true);
-	contact.phoneNumbers = phoneNums;
-	
-	var emailAddresses = [1];
-	emailAddresses[0] = new ContactField('home', details.EmailAddress, true);
-	contact.emails = emailAddresses;
-	
-	contact.save(function(){alert('New contact was saved'); $.mobile.changepage("#home",{ transition:"slideup"}); }, function(){alert('Contact couldn\'t saved');});
-}
-
-function searchContact()
-{
-	var name = document.getElementById('contactNameSearch').value;
-	var opts = new ContactFindOptions();
-	opts.filter = name;
-	opts.multiple = true;
-	var fields = ["name","phoneNumbers"];
-	$('#contactsList').empty();
-	$('#contactsList').css('display','none');
-	navigator.contacts.find(fields, contactFindSuccess, function(){alert('Contact search error');}, opts);
-}
-function contactFindSuccess(contacts)
-{
-	var numContacts = contacts.length;
-	if(numContacts > 0)
-	{
-
-		//console.log(contacts);
-		var content = "";
-		
-		var theList = '<ul data-role="collapsible">';
-		for(var i =0; i<numContacts; i++)
-		{
-			theList += '<li><a href="#" data-role="button">'+contacts[i].name.givenName;
-			if(contacts[i].name.familyName != null)
-				theList +=' '+contacts[i].name.familyName;
-			theList +='</a>';
-			theList += '<ul data-role="listview"><li>'+contacts[i].name.givenName;
-			if(contacts[i].name.familyName != null)
-			{
-				theList += ' ' + contacts[i].name.familyName;
-			}
-			theList +='</li>';
-			if(contacts[i].phoneNumbers.length>0)
-			{
-				for(var x=0; x<contacts[i].phoneNumbers.length; x++)
-				{
-					theList +='<li>'+contacts[i].phoneNumbers[x].value+' <span style="font-style:italic">('+contacts[i].phoneNumbers[x].type+')</span></li>';
-				}
-			}
-			theList +='</ul>';
-			theList += '</li>';
-		}
-		theList += '</ul>';
-		$('#contactsList').html(theList).css('display','block');
-		
-		$('#contactsList li a').click(function(){
-			$(this).parent('li').children('ul').toggle();
-		});
-		contacts = "";
-		
-	}
-	else
-	{
-		alert('No contact found');
-	}
-	document.getElementById('contactNameSearch').value = "";
-}
-
-function takePicture()
-{
-	if(navigator.camera != null)
-	{
-		navigator.device.capture.captureImage(pictureSuccess, pictureError);
-	}
-	else
-	{
-		alert('Camera is not supported');
-	}
-}
-function pictureError(message)
-{
-	if(message.code != 3)
-	{
-		$('#imageStatus').html('Failed because: '+message.code).css('display','block');
-	}
-}
-function pictureSuccess(data)
-{
-	imageURI = data[0];
-	$('#imageStatus').html('Image taken').css('display','block');
-	$('#pictureUploadBtn').css('display','block');
-	
-}
-
- // alert dialog dismissed
-function alertDismissed() {
-        // do something
+    if(file.files[0]) // if there is a photo
+    {
+        formData.append('postPhoto', file.files[0]);  // append photo file into form
     }
 
+    client.upload.onprogress = function(progressEvent)
+    {
+        $('#loader').show();
 
-function uploadPicToServer()
+        if(progressEvent.lengthComputable){
+            var perc = Math.floor(progressEvent.loaded / progressEvent.total*100);
+            $('#points').css('width', perc*0.9+"%");
+            $('#points').html(perc+"% done");
+        }
+    };
+
+    client.open("POST","http://vancouverwebschool.com/webblog/admin/handler.php?addPost", true); // open socket
+    client.send(formData); // sending form to server
+} // end of sendPost function
+
+client.onreadystatechange= function()  // listening ajax communication state changes
 {
-	$('#imageStatus').html('Sending image').css('color','#F90');
-	
-	var opts = new FileUploadOptions();
-	opts.fileKey = "file";
-	opts.fileName = device.name;
-	opts.mimeType = "image/jpeg";
-	
-	var ft = new FileTransfer();
-	ft.upload(imageURI.fullPath, encodeURI("http://theiamzone.com/ali_efe/mobile-app/api-port.php"), picUploadSuccess, picUploadFail, opts);
-	
-	navigator.camera.cleanup();
-	$('#pictureUploadBtn').css('display','none');
-	
+    if(client.readyState == 4 && client.status == 200) // when there is a result for savePost (success)
+    {
+        $('#loader').hide();
+
+        var data = JSON.parse(client.responseText); // receiving server's answer and convert to json object
+        if(data.result == true) // if post saved successfully
+        {
+            alert(data.msg);
+            $('#postContent').val('');
+            $('#postPhoto').val('');
+            $('#points').css('width','0'); // resetting progress bar
+
+            $.mobile.changePage( "#home", { transition: "slideup", changeHash: false });
+        }
+        else // if saving post failed
+        {
+            alert(data.msg);
+        }
+    }
+
 }
-function picUploadSuccess(r)
+
+
+
+
+function watchLocation(position)
 {
-	console.log("Code = "+r.responseCode);
-	console.log("Response = " + r.response);
-	console.log("Sent = " + r.bytesSent);
-	if(r.response == 1)
-	{
-		$('#imageStatus').html('Image uploaded').css('color','#393');
-	}
-	else if(r.response == 0)
-	{
-		$('#imageStatus').html('File uploaded but couldn\'t saved on server').css('color','red');
-	}
-	else
-	{
-		$('#imageStatus').html('File couldn\'t uploaded').css('color','red');
-	}
+    user.lat = position.coords.latitude;
+    user.long = position.coords.longitude;
 }
-function picUploadFail(error)
+function locationError(e)
 {
-	$('#imageStatus').html('something went wrong').css('color','#C60');
+    switch (e.code)
+    {
+        case 0: console.log('Something went wrong '+ e.message);
+            break;
+        case 1: console.log('You denied permission to retrieve a location');
+            break;
+        case 2: console.log('Browser was unable to retrieve your location');
+            break;
+        case 3: console.log('Browser timed out before retrieving the location');
+            break;
+    }
+}
+
+
+
+function getPosts(){
+
+    if(online == true)
+    {
+
+        $.ajax({
+            url:"http://vancouverwebschool.com/webblog/admin/handler.php",
+            cache:false,
+            data:{getPosts:true},
+            dataType: "JSON",
+            type:"GET",
+            crossDomain:true,
+            timeout:3000,
+            success: function(records)
+            {
+                if(records.result == false)
+                {
+                    alert(records.msg);
+                }
+                else
+                {
+                    var content = "";
+                    var counter = 0;
+
+                    $('#home .ui-content').html(''); // remove all existing article elements from home page
+
+                    $(records.data).each(function(index, row){
+                        content += '<article class="ui-body ui-body-a ui-corner-all">';
+                        content += '<h4>From: '+ row.userName+ ' <span class="articleDate">@ '+ row.date+'</span></h4>';
+                        content += '<p>';
+                        if(row.filename != null)
+                        {
+                            content += '<img src="http://vancouverwebschool.com/webblog/photos/'+row.filename+'" >';
+                        }
+                        content += row.content + '</p></article>';
+
+                        /// store this post into local tables
+                        if(localdbReady && counter <20)  // if there is a localdatabase and if counter is still less than 20
+                        {
+                            localdb.transaction(function(tx){
+                               tx.executeSql("INSERT INTO posts (`postID`,`content`,`date`,`authorID`, `comments`, `locLatitude`, `locLongitude`) VALUES (?,?,?,?,?,?,?)", [row.postID, row.content, row.date, row.authorID, row.comments,row.locLatitude, row.locLongitude]);
+                            });
+
+                            localdb.transaction(function(tx){
+                                tx.executeSql("INSERT INTO photos (`photoID`, `filename`, `postID`) VALUES (?,?,?)", [row.photoID, row.filename, row.photoPostID]);
+
+                            });
+
+                            localdb.transaction(function(tx){
+                                tx.executeSql("INSERT INTO users (`userID`,`userName`) VALUES (?,?)", [row.userID, row.userName]);
+                            });
+                            counter++;
+                        } // ends if condition
+
+                    }); // end of each loop
+                    $('#home .ui-content').html(content);
+                }
+            }
+        })
+    }// if ended here
+    else // if there is no internet connection
+    {
+
+        // get posts from local database
+
+    }
 }
